4693673a0fd022547064e3d6399ad6812db296c8
Jennifer Morgan <jemorgaine@gmail.com>
2020-12-09 17:16:37 -0600

    -m update with basic structure

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,12 @@
 public class Menu {
+    //menuItemArrayList - list menu items as hashmaps?
+    // addItemToMenu(itemName, price, description, category)
+    // addItemToMenu()
+    // removeItemFromMenu()
+    // isNew()
+    // daysSinceLastUpdate()
+    // isSelected = false
+    // printSelected()
+    // printALl()
+    // isEqual()
 }
--- src/MenuItem.java
+++ src/MenuItem.java
@@ -1,2 +1,10 @@
 public class MenuItem {
+    //itemName
+    //price
+    //description
+    //category {appetizer, main course, dessert}
+    //dateAddedToMenu
+    //dateOfLastUpdate
+
+
 }
